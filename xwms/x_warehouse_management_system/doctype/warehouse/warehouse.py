601da77a53be7fd027f907974e15fa8c59b90456
symonmuchemi <simonkenya846@gmail.com>
2025-05-17 04:17:27 +0300

    feat: Implement parent validation in Warehouse DocType
    Added the before_save method to the Warehouse class to validate the parent warehouse. The validate_parent method checks if the specified parent warehouse is a valid group. If not, it raises an error, ensuring data integrity when saving warehouse records.

--- xwms/x_warehouse_management_system/doctype/warehouse/warehouse.py
+++ xwms/x_warehouse_management_system/doctype/warehouse/warehouse.py
@@ -1,9 +1,20 @@
 # Copyright (c) 2025, SymonMuchemi and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.utils.nestedset import NestedSet
 
 
 class Warehouse(NestedSet):
-	pass
+    def before_save(self):
+        self.validate_parent()
+
+    def validate_parent(self):
+        parent_is_group = frappe.db.get_value(
+            "warehouse", self.parent_warehouse, "is_group"
+        )
+        if parent_is_group == 0:
+            print(f"is group value: {parent_is_group}")
+            frappe.throw(
+                f"Invalid operation, {self.parent_warehouse} is not a valid parent!"
+            )
