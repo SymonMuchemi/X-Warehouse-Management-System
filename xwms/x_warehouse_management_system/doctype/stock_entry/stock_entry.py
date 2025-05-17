ecca5e60dbd85aad697d3cc65a7cd696b854e1e6
symonmuchemi <simonkenya846@gmail.com>
2025-05-17 18:36:57 +0300

    feat: Implement validation logic for Stock Entry document
    Added comprehensive validation checks in the StockEntry class to ensure data integrity before saving. The validate method now checks for the presence of items, validates item quantities, and verifies warehouse conditions based on the type of stock entry (Receipt, Consume, Transfer). Specific error messages are thrown for various validation failures to guide users in correcting their input.

--- xwms/x_warehouse_management_system/doctype/stock_entry/stock_entry.py
+++ xwms/x_warehouse_management_system/doctype/stock_entry/stock_entry.py
@@ -1,9 +1,84 @@
 # Copyright (c) 2025, SymonMuchemi and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class StockEntry(Document):
-	pass
+    def before_save(self):
+        self.validate()
+
+    def validate(self):
+        if not self.items:
+            frappe.throw("Please add at least one item to the Stock Entry!")
+
+        for row in self.items:
+            # ensure quantity > 0
+            if not row.quantity or row.quantity <= 0:
+                frappe.throw(f"Quantity must be greater than zero for {row.item}")
+
+            # ensure correct warehouse on entry
+            if self.type == "Receipt":
+                if not self.to_warehouse:
+                    frappe.throw(
+                        f"To warehouse is required for receipt item - {row.item}"
+                    )
+                if self.from_warehouse:
+                    frappe.thow(f"From warehouse must be blank for {row.item}")
+
+                # ensure warehouse is a leaf
+                is_group = frappe.db.get_value(
+                    "Warehouse", self.to_warehouse, "is_group"
+                )
+
+                if is_group:
+                    frappe.throw(
+                        f"To warehouse must be a leaf node (not a group) for item {row.item}"
+                    )
+
+            elif self.type == "Consume":
+                if not self.from_warehouse:
+                    frappe.throw(
+                        f"From warehouse is required for receipt item - {row.item}"
+                    )
+                if self.to_warehouse:
+                    frappe.thow(f"To warehouse must be blank for {row.item}")
+
+                # ensure warehouse is a leaf
+                is_group = frappe.db.get_value(
+                    "Warehouse", self.from_warehouse, "is_group"
+                )
+
+                if is_group:
+                    frappe.throw(
+                        f"From warehouse must be a leaf node (not a group) for item {row.item}"
+                    )
+
+            elif self.stock_entry_type == "Transfer":
+                if not self.from_warehouse or not self.to_warehouse:
+                    frappe.throw(
+                        f"Both From and To Warehouses are required for Transfer (item {row.item})"
+                    )
+                if self.from_warehouse == self.to_warehouse:
+                    frappe.throw(
+                        f"From and To Warehouses cannot be the same for item {row.item}"
+                    )
+
+                # ensure warehouse is a leaf
+                from_wh_is_group = frappe.db.get_value(
+                    "Warehouse", self.from_warehouse, "is_group"
+                )
+                to_wh_is_group = frappe.db.get_value(
+                    "Warehouse", self.from_warehouse, "is_group"
+                )
+
+                if from_wh_is_group:
+                    frappe.throw(
+                        f"From warehouse must be a leaf node (not a group) for item {row.item}"
+                    )
+
+                if to_wh_is_group:
+                    frappe.throw(
+                        f"To warehouse must be a leaf node (not a group) for item {row.item}"
+                    )
