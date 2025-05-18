3c98a762699666c7c0457b2fdab1c9705e000a4d
symonmuchemi <simonkenya846@gmail.com>
2025-05-18 13:59:08 +0300

    feat: Enhance Stock Balance Report with filters and data retrieval
    - Added filters for posting date, item, and warehouse to the Stock Balance Report.
    - Implemented the get_data function to retrieve stock data based on the specified filters.
    - Updated the execute function to return columns and data for the report.
    - Improved the get_columns function to include additional fields such as quantity, valuation rate, and stock value.

--- xwms/x_warehouse_management_system/report/stock_balance_report/stock_balance_report.js
+++ xwms/x_warehouse_management_system/report/stock_balance_report/stock_balance_report.js
@@ -2,7 +2,25 @@
 // For license information, please see license.txt
 
 frappe.query_reports["Stock Balance Report"] = {
-	"filters": [
-
-	]
+  filters: [
+    {
+      fieldname: "posting_date",
+      label: "Posting Date",
+      fieldtype: "Date",
+      default: frappe.datetime.nowdate(),
+      reqd: 1
+    },
+    {
+      fieldname: "item",
+      label: "Item",
+      fieldtype: "Link",
+      options: "Item"
+    },
+    {
+      fieldname: "warehouse",
+      label: "Warehouse",
+      fieldtype: "Link",
+      options: "Warehouse"
+    }
+  ]
 };
--- xwms/x_warehouse_management_system/report/stock_balance_report/stock_balance_report.py
+++ xwms/x_warehouse_management_system/report/stock_balance_report/stock_balance_report.py
@@ -1,9 +1,77 @@
 # Copyright (c) 2025, SymonMuchemi and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 
 
 def execute(filters=None):
-	columns, data = [], []
-	return columns, data
+    filters = filters or {}
+    columns = get_columns()
+    data = get_data(filters)
+    return columns, data
+
+
+def get_columns():
+    return [
+        {
+            "label": "Item",
+            "fieldname": "item",
+            "fieldtype": "Link",
+            "options": "Item",
+            "width": 150,
+        },
+        {
+            "label": "Warehouse",
+            "fieldname": "warehouse",
+            "fieldtype": "Link",
+            "options": "Warehouse",
+            "width": 120,
+        },
+        {"label": "Quantity", "fieldname": "qty", "fieldtype": "Float", "width": 120},
+        {
+            "label": "Valuation Rate",
+            "fieldname": "valuation_rate",
+            "fieldtype": "Currency",
+            "width": 150,
+        },
+        {
+            "label": "Stock Value",
+            "fieldname": "stock_value",
+            "fieldtype": "Currency",
+            "width": 150,
+        },
+    ]
+
+
+def get_data(filters):
+    conditions = []
+
+    if filters.get("item"):
+        conditions.append("item = %(item)s")
+
+    if filters.get("warehouse"):
+        conditions.append("warehouse = %(warehouse)s")
+
+    if filters.get("posting_date"):
+        conditions.append("posting_date <= %(posting_date)s")
+
+    where_clause = "WHERE " + " AND ".join(conditions) if conditions else ""
+
+    query = f"""
+        SELECT 
+            item,
+            warehouse,
+            SUM(actual_quantity) AS qty,
+            CASE 
+                WHEN SUM(actual_quantity) = 0 THEN 0
+                ELSE SUM(actual_quantity * valuation_rate) / SUM(actual_quantity)
+            END AS valuation_rate,
+            SUM(actual_quantity * valuation_rate) AS stock_value
+        FROM `tabStock Ledger Entry`
+        {where_clause}
+        GROUP BY item, warehouse
+        HAVING qty != 0
+        ORDER BY item, warehouse
+    """
+
+    return frappe.db.sql(query, filters, as_dict=True)
